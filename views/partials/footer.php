cb458d336360a1c326831c1026c3157141c2e43d
Björn Folbert <bjorn.folbert@kan.se>
2021-12-06 13:57:02 +0100

    Update about text

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-12 text-center">
-        🎅<button data-bs-toggle="modal" data-bs-target="#info-modal" class="btn btn-link">About merrychristmasandahappynewyear.com</button>🎅
+        🎅<button data-bs-toggle="modal" data-bs-target="#info-modal" class="btn btn-link">About this project</button>🎅
       </div>
     </div>
   </div>
--- views/partials/info-modal.php
+++ views/partials/info-modal.php
@@ -7,17 +7,17 @@
       </div>
       <div class="modal-body">
 
-        <p>Made by <a href="https://folbert.com">Folbert</a>, probably the best Bear in the world<sup>&trade;</sup>, using <a href="https://www.strava.com/athletes/2962650">Strava</a> on December XX, 2021.</p>
+        <p>Made by <a href="https://folbert.com">Folbert</a>, probably the best Bear in the world<sup>&trade;</sup>, using <a href="https://www.strava.com/athletes/2962650">Strava</a> between December XX and YY, 2021.</p>
 
-        <p>All activities were run around <a href="https://sv.wikipedia.org/wiki/S%C3%A4rsl%C3%B6v_och_Djursl%C3%B6v">Särslöv, Djurslöv</a> and <a href="https://sv.wikipedia.org/wiki/Tottarp">Tottarp</a>. Most of them was done on <a href="https://skane.naturskyddsforeningen.se/hittaut/sydvastra-skane/kolbora-mosse/">Kolböra Mosse</a> / <a href="https://staffanstorp.se/fritid-och-upplevelser/natur-kultur-och-sevart/betrador/">Beträdorna</a>.</p>
+        <p>All activities were run around <a href="https://sv.wikipedia.org/wiki/S%C3%A4rsl%C3%B6v_och_Djursl%C3%B6v">Särslöv, Djurslöv</a> and <a href="https://sv.wikipedia.org/wiki/Tottarp">Tottarp</a>. A large portion of them were done on <a href="https://skane.naturskyddsforeningen.se/hittaut/sydvastra-skane/kolbora-mosse/">Kolböra Mosse</a> a.k.a. <a href="https://staffanstorp.se/fritid-och-upplevelser/natur-kultur-och-sevart/betrador/">Beträdorna</a>.</p>
 
         <p>Total letter distance ~<?php echo Words::get_total_distance(); ?> km (~<?php echo Words::get_total_distance(Words::DISTANCE_UNIT_MILES) ?> miles).</p>
 
-        <p>Code available at <a href="https://github.com/folbert/merrychristmasandahappynewyear-2021">folbert/merrychristmasandahappynewyear-2021</a>.</p>
+        <p>The complete code for the site is available at <a href="https://github.com/folbert/merrychristmasandahappynewyear-2021">folbert/merrychristmasandahappynewyear-2021</a> on GitHub.</p>
 
-        <p>More GPS-art is available at <a href="https://www.strav.art/">https://www.strav.art/</a>.</p>
+        <p>More GPS-art can be found at, for example, <a href="https://www.strav.art/">https://www.strav.art/</a>.</p>
 
-        <p>This site has, in one form or another, been wishing you a Merry Christmas And A Happy New Year all year round since November 26th, 2004.</p>
+        <p>merrychristmasandahappynewyear.com has, in one form or another, been wishing you a Merry Christmas And A Happy New Year all year round since November 26th, 2004.</p>
 
         <p>Sorry Santa, there are no cookies here.</p>
       </div>
