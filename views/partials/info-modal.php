1f91f3d81b0238b76653ed196a948e29e5cce37f
Björn Folbert <bjorn.folbert@kan.se>
2021-12-07 21:52:07 +0100

    Change about text and some styling for that

--- views/partials/info-modal.php
+++ views/partials/info-modal.php
@@ -7,17 +7,27 @@
       </div>
       <div class="modal-body">
 
-        <p>Made by <a href="https://folbert.com">Folbert</a>, probably the best Bear in the world<sup>&trade;</sup>, using <a href="https://www.strava.com/athletes/2962650">Strava</a> between December XX and YY, 2021.</p>
-
-        <p>All activities were run around <a href="https://sv.wikipedia.org/wiki/S%C3%A4rsl%C3%B6v_och_Djursl%C3%B6v">Särslöv, Djurslöv</a> and <a href="https://sv.wikipedia.org/wiki/Tottarp">Tottarp</a>. A large portion of them were done on <a href="https://skane.naturskyddsforeningen.se/hittaut/sydvastra-skane/kolbora-mosse/">Kolböra Mosse</a> a.k.a. <a href="https://staffanstorp.se/fritid-och-upplevelser/natur-kultur-och-sevart/betrador/">Beträdorna</a>.</p>
+        <h3>How</h3>
+        <p>Each letter is made up of a running activity on Strava where the movements were tracked using GPS.</p>
 
         <p>Total letter distance ~<?php echo Lines::get_total_distance(); ?> km (~<?php echo Lines::get_total_distance(Lines::DISTANCE_UNIT_MILES) ?> miles).</p>
+
+        <h3>Who</h3>
+        <p>Made by <a href="https://folbert.com">Folbert</a>, probably the best Bear in the world<sup>&trade;</sup>, using <a href="https://www.strava.com/athletes/2962650">Strava</a> between December XX and YY, 2021.</p>
+
+        <h3>Where</h3>
+        <p>All activities were run around <a href="https://sv.wikipedia.org/wiki/S%C3%A4rsl%C3%B6v_och_Djursl%C3%B6v">Särslöv, Djurslöv</a> and <a href="https://sv.wikipedia.org/wiki/Tottarp">Tottarp</a>. A large portion of them were done on <a href="https://skane.naturskyddsforeningen.se/hittaut/sydvastra-skane/kolbora-mosse/">Kolböra Mosse</a> a.k.a. <a href="https://staffanstorp.se/fritid-och-upplevelser/natur-kultur-och-sevart/betrador/">Beträdorna</a>.</p>
+
+        <h3>Why</h3>
+        <p>I like web development and running and wanted to celebrate the holidays and the roads and paths in the area where I live.</p>
+
+        <h3>Misc.</h3>
 
         <p>The complete code for the site is available at <a href="https://github.com/folbert/merrychristmasandahappynewyear-2021">folbert/merrychristmasandahappynewyear-2021</a> on GitHub.</p>
 
         <p>More GPS-art can be found at, for example, <a href="https://www.strav.art/">https://www.strav.art/</a>.</p>
 
-        <p>merrychristmasandahappynewyear.com has, in one form or another, been wishing you a Merry Christmas And A Happy New Year all year round since November 26th, 2004.</p>
+        <p>merrychristmasandahappynewyear.com has, in one form or another, been wishing a Merry Christmas And A Happy New Year all year round since November 26th, 2004.</p>
 
         <p>Sorry Santa, there are no cookies here.</p>
       </div>
