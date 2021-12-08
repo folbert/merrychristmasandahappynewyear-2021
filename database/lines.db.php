4076db3bf377caa6dc56159c19ee5618287798f2
Björn Folbert <bjorn.folbert@kan.se>
2021-12-08 10:37:56 +0100

    Add activity data for all the current letters

--- database/lines.db.php
+++ database/lines.db.php
@@ -24,21 +24,21 @@ return [
       ],
       [
         'image_name' => 'r-1',
+        'distance' => '1.15',
+        'activity_id_1' => '6340620715',
+        'activity_id_2' => '07c5e26355012891dcb8410e88220576ac0e1cac',
+      ],
+      [
+        'image_name' => 'r-2',
         'distance' => '0.98',
         'activity_id_1' => '6340636460',
         'activity_id_2' => '81f7344c14a0f7b67730671f58dba417947fa244',
       ],
       [
-        'image_name' => 'r-2',
-        'distance' => '0',
-        'activity_id_1' => '',
-        'activity_id_2' => '',
-      ],
-      [
         'image_name' => 'y',
-        'distance' => '0',
-        'activity_id_1' => '',
-        'activity_id_2' => '',
+        'distance' => '1.43',
+        'activity_id_1' => '6340580393',
+        'activity_id_2' => 'ce8b5bfbf37c889a9d9bcd5ef7a6ad720f72e4d0',
       ],
       [
         'col_elm_class_attr' => 'd-none d-sm-block',
@@ -53,15 +53,15 @@ return [
     'letters' => [
       [
         'image_name' => 'c',
-        'distance' => '0',
-        'activity_id_1' => '',
-        'activity_id_2' => '',
+        'distance' => '0.22',
+        'activity_id_1' => '6340689592',
+        'activity_id_2' => 'ee330be0d53f8dcb84c172e3e29f9fa2c1fa73c4',
       ],
       [
         'image_name' => 'h',
-        'distance' => '0',
-        'activity_id_1' => '',
-        'activity_id_2' => '',
+        'distance' => '1.74',
+        'activity_id_1' => '6345141054',
+        'activity_id_2' => 'a946ed6c43ee6e79d6e7ac0eb1626ea81ee2ad6a',
       ],
       [
         'image_name' => 'r',
@@ -71,9 +71,9 @@ return [
       ],
       [
         'image_name' => 'i',
-        'distance' => '0',
-        'activity_id_1' => '',
-        'activity_id_2' => '',
+        'distance' => '0.6',
+        'activity_id_1' => '6340591680',
+        'activity_id_2' => 'add4e7e4ac575b6c5c8a69e8eef4ca39ab870fc7',
       ],
       [
         'image_name' => 's-1',
@@ -89,9 +89,9 @@ return [
       ],
       [
         'image_name' => 'm',
-        'distance' => '0',
-        'activity_id_1' => '',
-        'activity_id_2' => '',
+        'distance' => '0.82',
+        'activity_id_1' => '6345209014',
+        'activity_id_2' => 'b7e40f9f13ba268b0175073a4e51eb7b2846125b',
       ],
       [
         'image_name' => 'a',
@@ -172,9 +172,9 @@ return [
       ],
       [
         'image_name' => 'p-1',
-        'distance' => '0',
-        'activity_id_1' => '',
-        'activity_id_2' => '',
+        'distance' => '0.62',
+        'activity_id_1' => '6354013833',
+        'activity_id_2' => '79ce79bde6d60f6a40dfd216c3ad74d91416b356',
       ],
       [
         'image_name' => 'p-2',
@@ -184,9 +184,9 @@ return [
       ],
       [
         'image_name' => 'y',
-        'distance' => '0',
-        'activity_id_1' => '',
-        'activity_id_2' => '',
+        'distance' => '0.5',
+        'activity_id_1' => '6354051157',
+        'activity_id_2' => '2c733e57c8b65aea938ba992a32cad7aae398faf',
       ],
       [
         'col_elm_class_attr' => 'd-none d-sm-block',
@@ -207,9 +207,9 @@ return [
       ],
       [
         'image_name' => 'e-1',
-        'distance' => '0',
-        'activity_id_1' => '',
-        'activity_id_2' => '',
+        'distance' => '3.54',
+        'activity_id_1' => '6353963045',
+        'activity_id_2' => 'd6b61bec7ceff5072168e1dc5f2267bb16a7eca7',
       ],
       [
         'image_name' => 'w',
