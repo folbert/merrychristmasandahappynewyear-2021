cf65ace83925b094cbf3851e0adbcfdde9bd8984
Björn Folbert <bjorn.folbert@kan.se>
2021-12-21 08:38:23 +0100

    Change some text and add minor style changes

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-12 text-center">
-        Each letter is a running activity that was tracked using GPS.
+        Each of the letters above is a running activity that was tracked using GPS.
       </div>
       <div class="col-12 text-center">
         🎅<button data-bs-toggle="modal" data-bs-target="#info-modal" class="btn btn-link">More about this project</button>🎅
